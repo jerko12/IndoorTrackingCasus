0e097441d9197706d24c5a107754de40d8c82966
jerko12 <61011416+jerko12@users.noreply.github.com>
2021-01-12 16:28:57 +0100

    Update IndoorTracking.py

--- IndoorTracking.py
+++ IndoorTracking.py
@@ -3,6 +3,21 @@ import imutils
 from imutils.object_detection import non_max_suppression
 import numpy as np
 import pyrebase
+import datetime
+
+
+#Trackers
+trDict = {'csrt' : cv2.TrackerCSRT_create,
+'kcf' : cv2.TrackerKCF_create,
+'boosting' : cv2.TrackerBoosting_create,
+'mil' : cv.TrackerMIL_create,
+'tld' : cv2.TrackerTLD_create,
+'medianflow' : cv2.TrackerMedianFlow_create,
+'mosse' : cv2.TrackerMOSSE_create
+}
+
+
+roomName = "Room1"
 
 #Firebase
 firebaseConfig={
@@ -17,24 +32,29 @@ firebaseConfig={
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
-
 db = firebase.database()
-#auth = firebase.auth()
-#storage=firebase.storage()
 
+db.child("Rooms").child("Room1").child("12").child("0").set(3)
 
+hours = 0
+minutes = 0
 
-def getData():
-    return {
-        'PeopleCounts': {'12:00': 4,'12:01':3}
-    }
-
-data = getData()
-
-db.child("Rooms").child("Room1").set(data)
+#for hour in range(0,24):
+ #   for minute in range(0,60):
+  #      db.child("Rooms").child("Room1").child(str(hour)).child(str(minute)).set(-1)
 
 def databaseCreate():
-    print("createNewData")
+    print("CreateNewData")
+
+def databaseRead():
+    print("Read Database")
+    #db.child("Rooms").child(str(roomName)).child("13").child("40").get()
+
+def databaseUpdate(roomName,hour,minute,data):
+    print("Update Database " + str(roomName) + "   " + str(hour) +":" + str(minute) +" : " + str(data))
+    db.child("Rooms").child(str(roomName)).child(str(hour)).child(str(minute)).set(data)
+
+    
 
 #Open CV
 cap = cv2.VideoCapture("walking.avi")
@@ -86,43 +106,58 @@ def calculateAverage(average,averageCount,currentCount):
 
     return average
 
-ret, frame1 = cap.read()
-ret, frame2 = cap.read()
+
+def Reset():
+    global avarage
+    global averageCount
+    average = 0
+    averageCount = 0
+
+
+def Program():
+    
+
+    ret, frame1 = cap.read()
+    ret, frame2 = cap.read()
+    global average 
+    global averageCount
+    global result
+    lastTime = datetime.datetime.now()
+    while cap.isOpened():
+
+        frame1 = imutils.resize(frame1, width=min(400, frame1.shape[1]))
+        frame1 = imutils.resize(frame1, width=max(800, frame1.shape[1]))
+        result,weights = detector(frame1.copy())
+        result1 = len(result)
+        
+
+        average = calculateAverage(average, averageCount ,result1)
+        averageCount += 1
+        print(result1,average,averageCount)
+
+        currentTime = datetime.datetime.now()
+        if(lastTime.minute != currentTime.minute):
+            print("H: " + str(currentTime.hour) + "  M: " + str(currentTime.minute))
+            databaseUpdate(roomName,currentTime.hour,currentTime.minute,int(round(average)))
+            Reset()
+            lastTime = currentTime
+
+        setRectangles(frame1,result,weights)
+
+        frame1 = imutils.resize(frame1, width=max(600, frame1.shape[1]))
+        cv2.imshow("feed", frame1)
+        frame1 = frame2
+        ret, frame2 = cap.read()
+
+        if cv2.waitKey(40) == 27:
+            break
+
 
 average = 0
 averageCount = 0
-while cap.isOpened():
+global result
 
-    frame1 = imutils.resize(frame1, width=min(400, frame1.shape[1]))
-    frame1 = imutils.resize(frame1, width=max(800, frame1.shape[1]))
-    result,weights = detector(frame1.copy())
-    result1 = len(result)
-    
-    #diff = cv2.absdiff(frame1,frame2)
-    #gray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
-    #blur = cv2.GaussianBlur(gray,(5,5),0)
-    #_ , thresh = cv2.threshold(blur,20,255, cv2.THRESH_BINARY)
-    #dilated = cv2.dilate(thresh, None, iterations=3)
-    #contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    #cv2.drawContours(frame1,contours, -1, (0,255,0), 2)
-
-    #print (result1 , weights)
-    average = calculateAverage(average, averageCount ,result1)
-    averageCount += 1
-    print(result1,average,averageCount)
-
-    setRectangles(frame1,result,weights)
-    
-    
-
-    #frame1 = imutils.resize(frame1, width=max(600, frame1.shape[1]))
-    cv2.imshow("feed", frame1)
-    frame1 = frame2
-    ret, frame2 = cap.read()
-
-    if cv2.waitKey(40) == 27:
-        break
+Program()
 
 cv2.destroyAllWindows()
 cap.release()
